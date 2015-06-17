4117aeec39d32280e9617b8d05325c2da20aea5e
Patrick Dugan <patrick@nextdoor.com>
2015-06-17 16:56:52 -0700

    basic exercise detail screen
    very basic implementation of exercise detail screen with data being
    passed through from tableview cell

--- codepath-fitness/ExerciseDetailViewController.swift
+++ codepath-fitness/ExerciseDetailViewController.swift
@@ -10,26 +10,29 @@ import UIKit
 
 class ExerciseDetailViewController: UIViewController {
 
+    @IBOutlet weak var exerciseLabel: UILabel!
+    @IBOutlet weak var intensityLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var exerciseName: String!
+    var exerciseDuration: String!
+    var exerciseIntensity: String!
+    var exerciseDescription: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        exerciseLabel.text = exerciseName
+        intensityLabel.text = exerciseIntensity
+        durationLabel.text = exerciseDuration
+        descriptionLabel.text = exerciseDescription
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
